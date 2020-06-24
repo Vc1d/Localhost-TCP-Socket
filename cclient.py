3de80e6e67d9a010d2e75b64a06c140eaa2a0ef8
Vo1d <66044267+Vc1d@users.noreply.github.com>
2020-06-24 09:14:36 -0400

    Update cclient.py

--- cclient.py
+++ cclient.py
@@ -1,17 +1,22 @@
+# import the needed modules
 import threading
 import socket
 
+# create a new socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-HOST = '127.0.0.1'
-PORT = 1024
-ENCODING = 'utf-8'
+# define the constants
+HOST = '127.0.0.1'    # localhost
+PORT = 1024           # TCP Port I used, but you can use any port that doesn't server a great purpose
+ENCODING = 'utf-8'    # Encoding I used. You can use pretty much any encoding such as ascii, utf-16, etc.
 
+# Connect the client to the server by localhost and the TCP port
 client.connect((HOST, PORT))
 
+# Make the user input a username
 username = input("Enter your username\n")
 
-
+# Define a function to recieve messages
 def receive():
     while True:
         try:
@@ -25,13 +30,13 @@ def receive():
             client.close()
             break
 
-
+# Define a function to send messages to other clients
 def inputname():
     while True:
         msg = f'{username}: {input("")}'
         client.send(msg.encode(ENCODING))
 
-
+# Set up threading so you can receive and send at the same time (while loop doesn't allow anything else to run without it finishing first)
 rcv_thread = threading.Thread(target=receive)
 rcv_thread.start()
 
